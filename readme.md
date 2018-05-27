92c84802db3451f09814014be2f18a3963077e9f
Mytakeon <arthur.alglave@gmail.com>
2018-05-27 15:02:15 +0200

    AeroPy example working, some classes created but not functional yet

---
+++ readme.md
@@ -0,0 +1,3 @@
+Xfoil API to perform various analysis on 2D airfoils. 
+
+Based of [AeroPy](https://github.com/leal26/AeroPy)
--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 import logging
 
-path_xfoil = r'/home/arthur/Coding/Aerodynamics_py3/modules/xfoil.exe'
+path_xfoil = r'/home/arthur/Coding/Aerodynamics/modules/xfoil.exe'
 path_log = r'/home/arthur/Coding/Aerodynamics_py3/log.log'
 path_output = r'/home/arthur/Coding/Aerodynamics_py3/data/out'
 
